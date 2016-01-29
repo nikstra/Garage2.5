84e32287925ed2d97e48f000cfe79af80be0bf91
Niklas Strand <nikstra@users.noreply.github.com>
2016-01-29 13:19:48 +0100

    Add seed method and menu item

--- Garage2.5/Migrations/Configuration.cs
+++ Garage2.5/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace Garage2.Migrations
 {
+    using Garage2.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -27,6 +28,30 @@ namespace Garage2.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            context.Members.AddOrUpdate(
+                n => n.Name,
+                new Member { Name = "Mai", email = "Mai@example.com", mobile = "070-111111" },
+                new Member { Name = "Mouhannad", email = "Mouhannad@example.com", mobile = "070-222222" },
+                new Member { Name = "Niklas", email = "Niklas@example.com", mobile = "070-333333" }
+                );
+
+            context.VehicleTypes.AddOrUpdate(
+                t => t.Type,
+                new VehicleType { Type = "Bil" },
+                new VehicleType { Type = "Buss" },
+                new VehicleType { Type = "Båt" },
+                new VehicleType { Type = "Flygplan" },
+                new VehicleType { Type = "Motorcykel" }
+                );
+
+
+            context.vehicles.AddOrUpdate(
+                r => r.RegNumber,
+                new vehicle { RegNumber = "ABC 123", Colour = "röd", VehicleTypeId = 1, MemberId = 1, Model = "Volvo", Wheels = 4, ParkedTime = new DateTime(2016, 1, 28, 13, 24, 0), VehicleType = new VehicleType { Type = "Bil" } },
+                new vehicle { RegNumber = "BCD 456", Colour = "blå", VehicleTypeId = 1, MemberId = 1, Model = "Saab", Wheels = 4, ParkedTime = new DateTime(2016, 1, 24, 10, 12, 0), VehicleType = new VehicleType { Type = "Bil" } },
+                new vehicle { RegNumber = "CDE 789", Colour = "gul", VehicleTypeId = 1, MemberId = 1, Model = "Opel", Wheels = 4, ParkedTime = new DateTime(2016, 1, 29, 8, 56, 0), VehicleType = new VehicleType { Type = "Bil" } }
+                );
         }
     }
 }
